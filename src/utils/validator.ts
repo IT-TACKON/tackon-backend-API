f489e364638fe5c0e9c960b4e52da668499567cf
Philip Purwoko <philippurwoko123@gmail.com>
2021-09-26 15:10:25 +0700

    Update min 30 chars long for question & comment text

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -15,7 +15,7 @@ const currentPasswordRules: ValidationChain = makePasswordRules('currentPassword
 
 // Question Rules
 const titleRules: ValidationChain = body('title').isLength({ 'min': 10, 'max': 100 }).withMessage('Question title length must between 10-100 character')
-const textRules: ValidationChain = body('text').isLength({ 'min': 50 }).withMessage('Text must be descriptive and at least has 50 characters')
+const textRules: ValidationChain = body('text').isLength({ 'min': 25 }).withMessage('Question text must be descriptive and at least has 25 characters')
 
 export const registerRules: ValidationChain[] = [emailRules, usernameRules, passwordRules]
 export const loginRules: ValidationChain[] = [emailRules, passwordRules]
